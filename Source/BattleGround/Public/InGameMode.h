eddf0b1219f285830fe2432be8a2b06a2f721286
micalia <micalia0612@gmail.com>
2023-06-15 17:14:10 +0900

    .승리, 패배 위젯 구현중

---
+++ Source/BattleGround/Public/InGameMode.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "InGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLEGROUND_API AInGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+public:
+	AInGameMode();
+	virtual void BeginPlay() override;
+
+	class UUserWidget* winWidget;
+};
